2f9df2d57d0b2e10f1bd0501c607947c10bfba94
Gregor Joham <gregor.joham@knorr-bremse.com>
2018-05-24 15:15:06 +0200

    add code, tests and composer

---
+++ src/SecureSearch.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace kbATeam\DataProtection;
+
+/**
+ * Class kbATeam\DataProtection\SecureSearch
+ *
+ * Deterministic one-way encryption of unique sensitive data.
+ * See README.md for details!
+ *
+ * The code surrounded by codeCoverageIgnore tags is not supposed to be reached,
+ * except when it happens (e.g. in the unlikely event openssl doesn't know the cipher
+ * being used) you'd want to know what happened where and to stop and further
+ * execution.
+ *
+ * @category library
+ * @package  kbATeam\DataProtection
+ * @license  MIT
+ */
+class SecureSearch
+{
+    /**
+     * @const Encryption method used to encrypt the data.
+     *        Only CBC based ciphers are applicable here! See README.md for details.
+     */
+    const CIPHER = 'AES-256-CBC';
+
+    /**
+     * @const Hash method used for the IV derivation.
+     */
+    const HASH = 'SHA256';
+
+    /**
+     * @const Number of iterations for the Password-Based Key Derivation Function.
+     */
+    const PBKDF2_ITERATIONS = 64000;
+
+    /**
+     * @const Length of the key generated.
+     */
+    const KEY_LENGTH = 32;
+
+    /**
+     * One-way encryption of unique sensitive data.
+     *
+     * @param $data string The data to encrypt.
+     * @param $key  string The key to use for encryption.
+     * @return string The base64 encoded encrypted data.
+     * @throws \RuntimeException in case the determination of the IV length,
+     *              the IV derivation from the data, or the encryption fails.
+     */
+    public static function encrypt($data, $key)
+    {
+        /**
+         * Determine the length of the initialization vector for the given cipher.
+         */
+        $iv_length = openssl_cipher_iv_length(self::CIPHER);
+        /**
+         * Throw exception in case the IV length cannot be determined.
+         * Are you messing with the cipher used?
+         */
+        if (false === $iv_length) {
+            //@codeCoverageIgnoreStart
+            throw new \RuntimeException('error determining IV length for cipher');
+            //@codeCoverageIgnoreEnd
+        }
+        /**
+         * Use the hash of the key as salt for the IV creation.
+         * This can be anything as long as it's deterministic.
+         */
+        $salt = hash(self::HASH, $key);
+
+        /**
+         * Derive the initialization vector from the given data.
+         * Attention: binary content.
+         */
+        $iv = openssl_pbkdf2($data, $salt, $iv_length, self::PBKDF2_ITERATIONS, self::HASH);
+        /**
+         * Throw exception in case the IV derivation from the plain text failed.
+         */
+        if (false === $iv) {
+            //@codeCoverageIgnoreStart
+            throw new \RuntimeException('IV derivation from data failed');
+            //@codeCoverageIgnoreEnd
+        }
+        /**
+         * Encrypt the data.
+         * Options "0" means, that the result is base64 encoded.
+         */
+        $result = openssl_encrypt($data, self::CIPHER, $key, 0, $iv);
+        if (false === $result) {
+            //@codeCoverageIgnoreStart
+            throw new \RuntimeException('encryption of data failed');
+            //@codeCoverageIgnoreEnd
+        }
+        /**
+         * Make sure the contents of these variables isn't accessible anymore because
+         * they contain sensitive information that can compromise the security of the
+         * encryption.
+         */
+        $key_hash = $iv_length = $iv = '';
+        unset($key_hash, $iv_length, $iv);
+
+        return $result;
+    }
+
+    /**
+     * Generate a key for encryption.
+     *
+     * @return string Hexadecimal representation of the generated key.
+     * @throws \RuntimeException in case the key length is too weak or the key
+     *                           generation failed.
+     */
+    public static function generateKey()
+    {
+        /**
+         * Generate a secret key based on the defined key length.
+         */
+        $key_raw = openssl_random_pseudo_bytes(self::KEY_LENGTH, $is_strong);
+        /**
+         * Throw exception in case the random key generation failed.
+         */
+        if (true !== $is_strong || false === $key_raw) {
+            //@codeCoverageIgnoreStart
+            throw new \RuntimeException('key generation failed');
+            //@codeCoverageIgnoreEnd
+        }
+        return bin2hex($key_raw);
+    }
+}
---
+++ tests/SecureSearchTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\kbATeam\DataProtection;
+
+use kbATeam\DataProtection\SecureSearch;
+
+/**
+ * Class Tests\kbATeam\DataProtection\SecureSearchTest
+ *
+ * Test the secure search class.
+ *
+ * @category Tests
+ * @package  Tests\kbATeam\DataProtection
+ * @license  MIT
+ */
+class SecureSearchTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @const The social security number used in these tests.
+     */
+    const SSN = '1234567890';
+
+    /**
+     * @const Random key used to encrypt the social security number in these tests.
+     */
+    const KEY = '2116d1542ad7377a9395e22c8264b480cdf843b069c391ff02183179f9ff2446';
+
+    /**
+     * @const The encrypted social security number used to validate the code.
+     */
+    const ENCRYPTED = 'RoH2Bfuob46Mn+XX5TETBg==';
+
+    /**
+     * Assert that the same data and the same key always give the same result.
+     */
+    public function testEncryptMethodSuccessfully()
+    {
+        //use a fake social security number
+        $data = self::SSN;
+        $key = hex2bin(self::KEY);
+        $encrypted_data = SecureSearch::encrypt($data, $key);
+        $this->assertEquals(self::ENCRYPTED, $encrypted_data);
+    }
+
+    /**
+     * Assert that a key of the defined length is generated.
+     */
+    public function testKeyGeneration()
+    {
+        $key = SecureSearch::generateKey();
+        $this->assertEquals(SecureSearch::KEY_LENGTH*2, strlen($key));
+    }
+
+    /**
+     * Assert that the data cannot be decrypted.
+     */
+    public function testDecryption()
+    {
+        $data = (int) self::SSN;
+        $key = hex2bin(self::KEY);
+        $encrypted = SecureSearch::encrypt($data, $key);
+        $this->assertEquals(self::ENCRYPTED, $encrypted);
+        $decrypted = openssl_decrypt($encrypted, SecureSearch::CIPHER, $key);
+        $this->assertFalse($decrypted);
+    }
+
+    /**
+     * Assert that it takes more than a month to calculate a rainbow table of
+     * encrypted Austrian social security numbers in case the secret key has been
+     * compromised.
+     */
+    public function testTimeRainbowTable()
+    {
+        $postfix = '010170';
+        $key = hex2bin(self::KEY);
+        $start = time();
+        for ($i = 1000; $i < 1500; $i++) {
+            $data = sprintf("%'.04d%s", $i, $postfix);
+            SecureSearch::encrypt($data, $key);
+        }
+        $duration = time() - $start;
+        /**
+         * Multiply duration by 18, because we only calculated 500 of the 9000
+         * possible prefix number for the rainbow table for a single day.
+         * 2014 the average life expectancy of a human was 71.5 years. In order to
+         * calculate a complete rainbow table for all possible social security
+         * numbers for 72 years, we have to multiply that duration by 26.298 days.
+         * The result is the number of seconds it takes a single thread to calculate
+         * a complete rainbow table of all possible social security numbers of 72
+         * years.
+         */
+        $expected_rainbow_table = $duration * 18 * 26298;
+        $msg = 'This test fails, because it is possible for a single thread to'
+               .' calculate a complete rainbow table of all possible social security'
+               .' numbers in less than a month (30.4375 days).';
+        $this->assertGreaterThan(2629800, $expected_rainbow_table, $msg);
+    }
+}
